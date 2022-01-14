33aa76a95799b02a32c13919a93bc0b21834770f
felixx97 <abrahammfelixx@gmail.com>
2022-01-14 16:50:55 -0300

    Primeiro projeto em Flutter

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeWi(),
+    );
+  }
+}
+
+class HomeWi extends StatelessWidget {
+  const HomeWi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(child: Text("Abraão Félix")),
+    );
+  }
+}
